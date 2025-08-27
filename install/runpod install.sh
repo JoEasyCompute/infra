624b2ec893240abcb83d067b7baa1d926c18f944
JoEasyCompute <joseph@easycompute.co.uk>
2025-08-27 13:49:29 +0100

    modify runpod install

--- install/runpod install.sh
+++ install/runpod install.sh
@@ -5,12 +5,18 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 sudo apt install git apt-transport-https ca-certificates curl software-properties-common cmake build-essential dkms -y
 
+# install kernel 6.8
+sudo apt install --install-recommends linux-generic-hwe-22.04 -y
+
+# reboot once
+sudo reboot now
+
 # sudo apt purge '^nvidia.*' '^libnvidia.*' '^cuda.*'
 # sudo apt autoremove
 # sudo apt autoclean
 
 # install nvidia keyring
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
 sudo apt install gcc-12 g++-12
@@ -23,30 +29,23 @@ sudo update-alternatives --config gcc  # Choose gcc-12
 sudo update-alternatives --config g++  # Choose g++-12
 
 # sudo apt install nvidia-driver-575-open nvidia-dkms-575-open nvidia-utils-575 -y
-sudo apt -V -y install cuda-toolkit-12-9 libnvidia-compute-575 nvidia-dkms-575-open nvidia-utils-575 cudnn9-cuda-12
-
+# sudo apt -V -y install cuda-toolkit-12-9 libnvidia-compute-575 nvidia-dkms-575-open nvidia-utils-575 cudnn9-cuda-12
+sudo apt -y install nvidia-driver-580-open nvidia-dkms-580-open nvidia-utils-580
 
 # update all packages
 sudo apt-get update && sudo apt-get upgrade -y
+# reboot once
+sudo reboot now
 
 # download latest & install nvidia driver
-LATEST_VER=$(curl -s https://www.nvidia.com/Download/processFind.aspx | grep -oP '([0-9]{3}\.[0-9]{2}\.[0-9]{2})' | head -1) && \
-wget -q https://us.download.nvidia.com/XFree86/Linux-x86_64/${LATEST_VER}/NVIDIA-Linux-x86_64-${LATEST_VER}.run && \
-chmod +x NVIDIA-Linux-x86_64-${LATEST_VER}.run && \
-sudo systemctl isolate multi-user.target && \
-sudo ./NVIDIA-Linux-x86_64-${LATEST_VER}.run --silent --no-cc-version-check --disable-nouveau --dkms
+# LATEST_VER=$(curl -s https://www.nvidia.com/Download/processFind.aspx | grep -oP '([0-9]{3}\.[0-9]{2}\.[0-9]{2})' | head -1) && \
+# wget -q https://us.download.nvidia.com/XFree86/Linux-x86_64/${LATEST_VER}/NVIDIA-Linux-x86_64-${LATEST_VER}.run && \
+# chmod +x NVIDIA-Linux-x86_64-${LATEST_VER}.run && \
+# sudo systemctl isolate multi-user.target && \
+# sudo ./NVIDIA-Linux-x86_64-${LATEST_VER}.run --silent --no-cc-version-check --disable-nouveau --dkms
 
-#wget https://us.download.nvidia.com/XFree86/Linux-x86_64/575.64.05/NVIDIA-Linux-x86_64-575.64.05.run
-#sudo sh NVIDIA-Linux-x86_64-575.64.05.run
-
-# reboot once
-sudo reboot now
-
-# install kernel 6.8
-sudo apt install --install-recommends linux-generic-hwe-22.04 -y
-
-# reboot once
-sudo reboot now
+# wget https://us.download.nvidia.com/XFree86/Linux-x86_64/575.64.05/NVIDIA-Linux-x86_64-575.64.05.run
+# sudo sh NVIDIA-Linux-x86_64-575.64.05.run
 
 # create new volume for runpod-data
 sudo lvcreate -L 3000G -n runpoddata ubuntu-vg
@@ -59,5 +58,3 @@ sudo lvcreate -L 3000G -n runpoddata ubuntu-vg
 # sudo lvcreate -l 100%FREE -n runpoddata ubuntu-vg
 
 # sudo lvextend -r -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
-
-
