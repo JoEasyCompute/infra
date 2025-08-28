e02122b780c4eca8b5ab13ee5f7059a3a5d57227
ShadowZ0 <joseph.secert@gmail.com>
2025-08-28 11:11:50 +0100

    new file:   install.sh 	modified:   install/DiskWiping.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Move all install scripts to home directory
+mv ~/infra/gpucheck/*.sh ~
+mv ~/infra/IPMI* ~
+mv ~/infra/drivers/Intel ~
+mv ~/infra/install/*.sh ~
+mv ~/infra/.bashrc ~
+mv ~/infra/.aliases ~
+ln -s ~/.aliases ~/.bash_aliases
